9b0e50e439b6f17dc70c5a32d90c70894edc10df
Bill Kuker <bkuker@billkuker.com>
2020-11-04 15:25:22 -0500

    Log error to serial

--- Lolin-Pointer/src/Error.cpp
+++ Lolin-Pointer/src/Error.cpp
@@ -1,6 +1,10 @@
+#include <Arduino.h>
 #include "Error.h"
 
-Error::Error(int _code, std::string _msg) : code(_code), msg(_msg) {}
+Error::Error(int _code, std::string _msg) : code(_code), msg(_msg) {
+    Serial.print("Error: ");
+    Serial.println(msg.c_str());
+}
 
 int Error::getCode() { return code; }
 
